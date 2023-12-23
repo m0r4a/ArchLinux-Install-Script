c73f0ac93455b1c8124f064f2166e71e03a21eb4
m0r4a <105532562+m0r4a@users.noreply.github.com>
2023-12-23 12:54:42 -0600

    Update main.sh

--- main.sh
+++ main.sh
@@ -31,7 +31,10 @@ swapon $swap_p
 
 # Formatting root partition
 echo "Formatting the root partition ($root_p)..."
-mkfs.btrfs $root_p
+mkfs.btrfs -f $root_p
+
+# Cleaning the screen
+clear
 
 # Mounting the root partition 
 mount $root_p /mnt 
@@ -50,8 +53,71 @@ umount /mnt
 mount -o noatime,compress=lzo,space_cache=v2,subvol=@ $root_p /mnt
 
   # Creating the directories
+echo "Creating the directories for the subvolumes..."
 mkdir -p /mnt/{boot,home,.snapshots,var_log}
 
+echo "Mounting the subvolumes..."
 mount -o noatime,compress=lzo,space_cache=v2,subvol=@home $root_p /mnt/home
 mount -o noatime,compress=lzo,space_cache=v2,subvol=@snapshots $root_p /mnt/.snapshots
 mount -o noatime,compress=lzo,space_cache=v2,subvol=@var_log $root_p /mnt/var_log
+
+  # Mounting boot
+mount $boot_p /mnt/boot
+
+# Cleaning the screen
+clear
+
+# Installing the base directories with the microcode picking
+
+echo "Please, select which microcode you want to install"
+
+select microcode_option in "amd-ucode" "intel-ucode" "None"; do
+    case $microcode_option in
+        "amd-ucode")
+            microcode_package="amd-ucode"
+            break
+            ;;
+        "intel-ucode")
+            microcode_package="intel-ucode"
+            break
+            ;;
+        "None")
+            microcode_package=""
+            break
+            ;;
+        *)
+            echo "Please, select a valid option"
+            ;;
+    esac
+done
+
+# Executing the command
+pacstrap /mnt base linux linux-firmware vim $microcode_package\
+
+# Generating the fstab 
+genfstab -U /mnt >> /mnt/etc/fstab 
+
+# Getting into the install
+arch-chroot /mnt
+
+# Creating the zoneinfo
+read -p "¿Do you know your zoneinfo? (S/N): " know_timezone
+
+case $know_timezone in
+    [SsYy]) 
+        # If the user knows the ZoneInfo, ask for it
+        read -p "Write your zoneinfo (e.g 'America/Mexico_City'): " timezone
+        ;;
+    *)
+        # If the user doesnt know its zoneinfo, they're shown
+        echo "Zonas horarias disponibles:"
+        timedatectl list-timezones
+        echo
+        read -p "Ingresa tu zona horaria: " timezone
+        ;;
+esac
+
+# Creating the symlink 
+sudo ln -sf "/usr/share/zoneinfo/$timezone" /etc/localtime
+
+
