bc50d9b8a71af06f9d0f4a02a872892fbf127864
m0r4a <105532562+m0r4a@users.noreply.github.com>
2024-01-04 17:20:32 -0600

    Update post-install-config.sh

--- post-install-config.sh
+++ post-install-config.sh
@@ -25,5 +25,29 @@ sudo mount -a
 
 sudo chmod 750 /.snapshots
 
-## Configuring snapper 
+## Configuring snapper timeline
 
+### Adding your username 
+sudo sed -i "s/ALLOW_USERS=\"\"/ALLOW_USERS=\"$USER\"/" /etc/snapper/configs/root
+
+### Recommended cleanup on the arch wiki
+sudo sed -i 's/TIMELINE_LIMIT_HOURLY="[0-9]\+"/TIMELINE_LIMIT_HOURLY="5"/' /etc/snapper/configs/root
+sudo sed -i 's/TIMELINE_LIMIT_DAILY="[0-9]\+"/TIMELINE_LIMIT_DAILY="7"/' /etc/snapper/configs/root
+sudo sed -i 's/TIMELINE_LIMIT_WEEKLY="[0-9]\+"/TIMELINE_LIMIT_WEEKLY="0"/' /etc/snapper/configs/root
+sudo sed -i 's/TIMELINE_LIMIT_MONTHLY="[0-9]\+"/TIMELINE_LIMIT_MONTHLY="0"/' /etc/snapper/configs/root
+sudo sed -i 's/TIMELINE_LIMIT_YEARLY="[0-9]\+"/TIMELINE_LIMIT_YEARLY="0"/' /etc/snapper/configs/root
+
+### Enable the timeline
+sudo systemctl enable --now snapper-timeline.timer
+sudo systemctl enable --now snapper-cleanup.timer
+
+## Clean the terminal
+clean 
+
+## Pospuesto
+echo "Installing yay..."
+cd /opt
+sudo git clone https://aur.archlinux.org/yay-git.git
+sudo chown -R $USER:$USER ./yay-git
+cd yay-git
+makepkg -si
