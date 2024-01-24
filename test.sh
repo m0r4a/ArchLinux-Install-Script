8da345bf5855392c9fb296ff4e3b634d8c8168f7
m0r4a <105532562+m0r4a@users.noreply.github.com>
2024-01-23 21:46:05 -0600

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,6 +1,11 @@
-## Getting the root partition 
+#!/bin/bash
+
+# Getting the root partition
 p_root=$(awk '/\s\/\s/ {print prev} {prev = $0}' /etc/fstab | sed 's/^#\s*//')
 
-## Adding the root partition to the snapper-rollback config file
+# Adding the root partition to the snapper-rollback config file
+sudo -v # Solicitar la contraseña de sudo de antemano
+
+echo "Adding the root partition to snapper-rollback config file..."
 sudo sed -i '/^#dev/d' /etc/snapper-rollback.conf
-echo "dev = $p_root" | sudo cat >> /etc/snapper-rollback.conf
+echo "dev = $p_root" | sudo tee -a /etc/snapper-rollback.conf
